ffe05358df0c21c9b678350c46dbdd11613fac2a
Liam-Wirth <liam.wirth1@outlook.com>
2022-06-12 21:21:38 -0700

    solved praclist

---
+++ ltime108/264.praclist.java
@@ -0,0 +1,13 @@
+package ltime108;
+// 6/12/2022
+import java.util.Scanner;
+
+class Codechef{
+    public static void main(String[] args) throws java.lang.Exception{
+        //your code goes here
+        Scanner input = new Scanner(System.in);
+        int allProb = input.nextInt();
+        int attemptProb = input.nextInt();
+        System.out.println(allProb-attemptProb);
+    }
+}
