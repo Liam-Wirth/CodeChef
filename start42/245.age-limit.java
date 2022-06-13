b2f7aacbda60397051ced180802e4791b5ffde40
Liam-Wirth <liam.wirth1@outlook.com>
2022-06-12 21:11:34 -0700

    solved age limit

---
+++ start42/245.age-limit.java
@@ -0,0 +1,24 @@
+package start42;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner input = new Scanner(System.in);
+        int run = input.nextInt();
+        while(run>0){
+        int X = input.nextInt();
+        int Y = input.nextInt();
+        int A = input.nextInt();
+        if (A>=X && A<Y){
+            System.out.println("YES");
+        }else System.out.println("NO");
+        run--;
+        }
+	}
+}
